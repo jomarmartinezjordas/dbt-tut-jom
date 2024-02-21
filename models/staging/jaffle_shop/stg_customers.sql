2e00177645487230e1c6e08d6c11a70284574280
Jomar Jordas <jomarmartinezjordas@gmail.com>
2024-02-21 09:27:50 +0000

    configure ad select from sources - added version 2 of stg_customers and stg_orders - added src_jaffle_shop.yml file - changed the source reference using {{ source( 'schema' , 'table' ) }}

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,13 +1,37 @@
-with customers as (
+--------VERSION 2----------
+
+with 
+
+source as (
+
+    select *
+    from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+staged as (
 
     select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
-
+         id as customer_id
+        ,first_name
+        ,last_name
+    from source
 )
 
-select * 
-from customers
+select * from staged
+
+--------VERSION 1----------
+
+-- with customers as (
+
+--     select
+--         id as customer_id,
+--         first_name,
+--         last_name
+
+--     from raw.jaffle_shop.customers
+
+-- )
+
+-- select * 
+-- from customers
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,14 +1,40 @@
-with orders as (
+--------VERSION 2----------
+
+with
+
+source as (
+
+    select *
+    from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+staged as (
 
     select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+         id as order_id
+        ,user_id as customer_id
+        ,order_date
+        ,status
+    from source
 
 )
 
-select * 
-from orders
+select * from staged
+
+--------VERSION 1----------
+
+-- with orders as (
+
+--     select
+--         id as order_id,
+--         user_id as customer_id,
+--         order_date,
+--         status
+
+--     from raw.jaffle_shop.orders
+
+-- )
+
+-- select * 
+-- from orders
